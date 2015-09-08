345951327a0afd6941eeab4e9ff10ab02df51689
Aaron Niskin <amniskin@gmail.com>
2015-09-08 01:20:07 -0400

    GA fixed?

--- book/sicp/index.html.out/general/head.js
+++ book/sicp/index.html.out/general/head.js
@@ -8,22 +8,22 @@ goog.require('tailrecursion.javelin');
  */
 general.head.bootstrap = (function() { 
 var general$head$bootstrap__delegate = function (args__8262__auto__){
-var vec__272584 = tailrecursion.hoplon.parse_args(args__8262__auto__);
-var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__272584,(0),null);
-var ___$1 = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__272584,(1),null);
+var vec__355718 = tailrecursion.hoplon.parse_args(args__8262__auto__);
+var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__355718,(0),null);
+var ___$1 = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__355718,(1),null);
 return (tailrecursion.hoplon.link.cljs$core$IFn$_invoke$arity$6 ? tailrecursion.hoplon.link.cljs$core$IFn$_invoke$arity$6(cljs.core.cst$kw$REL,"stylesheet",cljs.core.cst$kw$href,"/css/bootstrap.css",cljs.core.cst$kw$type,"text/css") : tailrecursion.hoplon.link.call(null,cljs.core.cst$kw$REL,"stylesheet",cljs.core.cst$kw$href,"/css/bootstrap.css",cljs.core.cst$kw$type,"text/css"));
 };
 var general$head$bootstrap = function (var_args){
 var args__8262__auto__ = null;
 if (arguments.length > 0) {
-var G__272585__i = 0, G__272585__a = new Array(arguments.length -  0);
-while (G__272585__i < G__272585__a.length) {G__272585__a[G__272585__i] = arguments[G__272585__i + 0]; ++G__272585__i;}
-  args__8262__auto__ = new cljs.core.IndexedSeq(G__272585__a,0);
+var G__355719__i = 0, G__355719__a = new Array(arguments.length -  0);
+while (G__355719__i < G__355719__a.length) {G__355719__a[G__355719__i] = arguments[G__355719__i + 0]; ++G__355719__i;}
+  args__8262__auto__ = new cljs.core.IndexedSeq(G__355719__a,0);
 } 
 return general$head$bootstrap__delegate.call(this,args__8262__auto__);};
 general$head$bootstrap.cljs$lang$maxFixedArity = 0;
-general$head$bootstrap.cljs$lang$applyTo = (function (arglist__272586){
-var args__8262__auto__ = cljs.core.seq(arglist__272586);
+general$head$bootstrap.cljs$lang$applyTo = (function (arglist__355720){
+var args__8262__auto__ = cljs.core.seq(arglist__355720);
 return general$head$bootstrap__delegate(args__8262__auto__);
 });
 general$head$bootstrap.cljs$core$IFn$_invoke$arity$variadic = general$head$bootstrap__delegate;
@@ -35,22 +35,22 @@ return general$head$bootstrap;
  */
 general.head.viewport = (function() { 
 var general$head$viewport__delegate = function (args__8262__auto__){
-var vec__272588 = tailrecursion.hoplon.parse_args(args__8262__auto__);
-var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__272588,(0),null);
-var ___$1 = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__272588,(1),null);
+var vec__355722 = tailrecursion.hoplon.parse_args(args__8262__auto__);
+var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__355722,(0),null);
+var ___$1 = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__355722,(1),null);
 return (tailrecursion.hoplon.html_meta.cljs$core$IFn$_invoke$arity$4 ? tailrecursion.hoplon.html_meta.cljs$core$IFn$_invoke$arity$4(cljs.core.cst$kw$name,"viewport",cljs.core.cst$kw$content,"width=device-width, initial-scale=1") : tailrecursion.hoplon.html_meta.call(null,cljs.core.cst$kw$name,"viewport",cljs.core.cst$kw$content,"width=device-width, initial-scale=1"));
 };
 var general$head$viewport = function (var_args){
 var args__8262__auto__ = null;
 if (arguments.length > 0) {
-var G__272589__i = 0, G__272589__a = new Array(arguments.length -  0);
-while (G__272589__i < G__272589__a.length) {G__272589__a[G__272589__i] = arguments[G__272589__i + 0]; ++G__272589__i;}
-  args__8262__auto__ = new cljs.core.IndexedSeq(G__272589__a,0);
+var G__355723__i = 0, G__355723__a = new Array(arguments.length -  0);
+while (G__355723__i < G__355723__a.length) {G__355723__a[G__355723__i] = arguments[G__355723__i + 0]; ++G__355723__i;}
+  args__8262__auto__ = new cljs.core.IndexedSeq(G__355723__a,0);
 } 
 return general$head$viewport__delegate.call(this,args__8262__auto__);};
 general$head$viewport.cljs$lang$maxFixedArity = 0;
-general$head$viewport.cljs$lang$applyTo = (function (arglist__272590){
-var args__8262__auto__ = cljs.core.seq(arglist__272590);
+general$head$viewport.cljs$lang$applyTo = (function (arglist__355724){
+var args__8262__auto__ = cljs.core.seq(arglist__355724);
 return general$head$viewport__delegate(args__8262__auto__);
 });
 general$head$viewport.cljs$core$IFn$_invoke$arity$variadic = general$head$viewport__delegate;
@@ -62,50 +62,50 @@ return general$head$viewport;
  */
 general.head.mathjax = (function() { 
 var general$head$mathjax__delegate = function (args__8262__auto__){
-var vec__272592 = tailrecursion.hoplon.parse_args(args__8262__auto__);
-var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__272592,(0),null);
-var ___$1 = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__272592,(1),null);
+var vec__355726 = tailrecursion.hoplon.parse_args(args__8262__auto__);
+var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__355726,(0),null);
+var ___$1 = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__355726,(1),null);
 return (tailrecursion.hoplon.script.cljs$core$IFn$_invoke$arity$4 ? tailrecursion.hoplon.script.cljs$core$IFn$_invoke$arity$4(cljs.core.cst$kw$type,"text/javascript",cljs.core.cst$kw$src,"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML") : tailrecursion.hoplon.script.call(null,cljs.core.cst$kw$type,"text/javascript",cljs.core.cst$kw$src,"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"));
 };
 var general$head$mathjax = function (var_args){
 var args__8262__auto__ = null;
 if (arguments.length > 0) {
-var G__272593__i = 0, G__272593__a = new Array(arguments.length -  0);
-while (G__272593__i < G__272593__a.length) {G__272593__a[G__272593__i] = arguments[G__272593__i + 0]; ++G__272593__i;}
-  args__8262__auto__ = new cljs.core.IndexedSeq(G__272593__a,0);
+var G__355727__i = 0, G__355727__a = new Array(arguments.length -  0);
+while (G__355727__i < G__355727__a.length) {G__355727__a[G__355727__i] = arguments[G__355727__i + 0]; ++G__355727__i;}
+  args__8262__auto__ = new cljs.core.IndexedSeq(G__355727__a,0);
 } 
 return general$head$mathjax__delegate.call(this,args__8262__auto__);};
 general$head$mathjax.cljs$lang$maxFixedArity = 0;
-general$head$mathjax.cljs$lang$applyTo = (function (arglist__272594){
-var args__8262__auto__ = cljs.core.seq(arglist__272594);
+general$head$mathjax.cljs$lang$applyTo = (function (arglist__355728){
+var args__8262__auto__ = cljs.core.seq(arglist__355728);
 return general$head$mathjax__delegate(args__8262__auto__);
 });
 general$head$mathjax.cljs$core$IFn$_invoke$arity$variadic = general$head$mathjax__delegate;
 return general$head$mathjax;
 })()
 ;
-general.head.google_analytics_text = "(function (i,s,o,g,r,a,m) {i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n\n  ga ('create', 'UA-67291006-1', 'auto');\n  ga ('send', 'pageview');";
+general.head.google_analytics_text = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n\n  ga ('create', 'UA-67291006-1', 'auto');\n  ga ('send', 'pageview');";
 /**
  * @param {...*} var_args
  */
 general.head.google_analytics = (function() { 
 var general$head$google_analytics__delegate = function (args__8262__auto__){
-var vec__272596 = tailrecursion.hoplon.parse_args(args__8262__auto__);
-var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__272596,(0),null);
-var ___$1 = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__272596,(1),null);
+var vec__355730 = tailrecursion.hoplon.parse_args(args__8262__auto__);
+var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__355730,(0),null);
+var ___$1 = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__355730,(1),null);
 return (tailrecursion.hoplon.script.cljs$core$IFn$_invoke$arity$1 ? tailrecursion.hoplon.script.cljs$core$IFn$_invoke$arity$1(general.head.google_analytics_text) : tailrecursion.hoplon.script.call(null,general.head.google_analytics_text));
 };
 var general$head$google_analytics = function (var_args){
 var args__8262__auto__ = null;
 if (arguments.length > 0) {
-var G__272597__i = 0, G__272597__a = new Array(arguments.length -  0);
-while (G__272597__i < G__272597__a.length) {G__272597__a[G__272597__i] = arguments[G__272597__i + 0]; ++G__272597__i;}
-  args__8262__auto__ = new cljs.core.IndexedSeq(G__272597__a,0);
+var G__355731__i = 0, G__355731__a = new Array(arguments.length -  0);
+while (G__355731__i < G__355731__a.length) {G__355731__a[G__355731__i] = arguments[G__355731__i + 0]; ++G__355731__i;}
+  args__8262__auto__ = new cljs.core.IndexedSeq(G__355731__a,0);
 } 
 return general$head$google_analytics__delegate.call(this,args__8262__auto__);};
 general$head$google_analytics.cljs$lang$maxFixedArity = 0;
-general$head$google_analytics.cljs$lang$applyTo = (function (arglist__272598){
-var args__8262__auto__ = cljs.core.seq(arglist__272598);
+general$head$google_analytics.cljs$lang$applyTo = (function (arglist__355732){
+var args__8262__auto__ = cljs.core.seq(arglist__355732);
 return general$head$google_analytics__delegate(args__8262__auto__);
 });
 general$head$google_analytics.cljs$core$IFn$_invoke$arity$variadic = general$head$google_analytics__delegate;
